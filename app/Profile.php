81992091f3a748fcc71e486a476901a9f4ac925a
THIlangarathna <50521314+THIlangarathna@users.noreply.github.com>
2020-03-21 20:51:44 +0530

    Initialize Commit

---
+++ app/Profile.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+
+    protected $guarded = [];
+
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : 'profile/dSYCS6X8O8Oc0QPPvzYyjOriX9HYRhZl88PubNm4.png';
+        return '/storage/' . $imagePath;
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,9 @@
+@component('mail::message')
+# Welcome
+
+Welcome to Insta!!!
+
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
